2d0116f6f4b610c232e872cce19ba070e2f95c02
Alex Roth <jonesroth7@gmail.com>
2019-08-23 11:50:06 -0600

    Add array of possible eight-ball replies

---
+++ index.js
@@ -0,0 +1,30 @@
+var possibleAnswers = [
+  "It is certain.",
+  "It is decidedly so.",
+  "Without a doubt.",
+  "Yes -definietly.",
+  "You may rely on it.",
+  "As I see it, yes.",
+  "Most likely.",
+  "Outlook good.",
+  "Yes.",
+  "Signs point to yes.",
+  "Reply hazy, try again.",
+  "As again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "concentrate and ask again.",
+  "Don't count on it.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook not so good.",
+  "Very doubtful."
+]
+
+var eightBallReply = possibleAnswers[Math.floor(Math.random()*possibleAnswers.length)];
+var getAnswer = document.querySelector(".ask-question-in");
+
+// function userQuestion() {
+//   return
+// }
+document.h1.innerText = eightBallReply;
